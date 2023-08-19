69d9031e66b95679047c13681ed1490642d5fd1d
RAWMIC17 <hks1710harsh@gmail.com>
2023-08-19 11:30:28 +0530

    First commit

---
+++ to_doapp/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Storing custom colors for use in the app
+
+const Color tdRed = Color(0xFFFF5733);
+const Color tdBlue = Color(0xFF0000FF);
+const Color tdBlack = Color(0xFF000000);
+const Color tdGrey = Color(0xFF808080);
+const Color tdBGColor = Color(0xFFEEEFF5);
